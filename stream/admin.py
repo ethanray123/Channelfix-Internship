3b9d7f3ef0bbfc9398dbcc651de55676c30cfea9
JoeBrotzer <jomar.brotzer@gmail.com>
2018-05-18 10:59:59 +0800

    created models in models.py

--- stream/admin.py
+++ stream/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class ListProfiles(admin.ModelAdmin):
+    list_display = ('owner', 'nickname')
+    list_filter = ['owner']
+
+
+class ListModerators(admin.ModelAdmin):
+    list_display = ('owner', 'lobby')
+    list_filter = ['owner']
+
+
+class ListComments(admin.ModelAdmin):
+    list_display = ('owner', 'lobby', 'when')
+    list_filter = ['when']
+
+
+class ListMembership(admin.ModelAdmin):
+    list_display = ('member', 'lobby')
+    list_filter = ['lobby']
+
+
+admin.site.register(Category)
+admin.site.register(Profile, ListProfiles)
+admin.site.register(Lobby)
+admin.site.register(Moderator, ListModerators)
+admin.site.register(Stream)
+admin.site.register(Comment, ListComments)
+admin.site.register(Lobby_Membership, ListMembership)
