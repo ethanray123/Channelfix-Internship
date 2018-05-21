8aa33a52bb952f1fba6c237a42c07ec8d1544a91
Ethan Ray Flores Mosqueda <ethanray.mosqueda@gmail.com>
2018-05-21 11:41:30 +0800

    edited settings.py to accommodate functionality

--- Project/settings.py
+++ Project/settings.py
@@ -1,1 +1,14 @@
 from local_settings import *
+
+STATIC_URL = '/static/'
+
+FIXTURE_DIRS = (
+    '/fixtures/',
+    '/stream/fixtures/',
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
