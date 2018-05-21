512c909a100e52a3232f5e202248d8a949a33796
rendave <rendave.lecciones3@gmail.com>
2018-05-21 20:04:40 +0800

    added home template view

--- stream/urls.py
+++ stream/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from stream.views import lobby
 
+app_name = 'stream'
+
 urlpatterns = [
     path('lobby/<int:pk>',
          lobby.DetailView.as_view(), name='lobby_detailview'),
--- stream/views/home.py
+++ stream/views/home.py
@@ -1,5 +1,11 @@
 from django.views import generic
+from stream import models
 
 
 class HomeView(generic.TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['lobbies'] = models.Lobby.objects.all()
+        return context
