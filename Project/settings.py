793cb8069c2126bef05983840b71d0a12afcad7d
JoeBrotzer <34963094+JoeBrotzer@users.noreply.github.com>
2018-05-21 12:11:56 +0800

    Moved local settings import to bottom of the file

--- Project/settings.py
+++ Project/settings.py
@@ -1,5 +1,4 @@
 import os
-from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,3 +114,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
+
+from .local_settings import *
