f41eb70ac1f6c39d1215de42ce3c8eeedce85711
Ethan Ray Flores Mosqueda <ethanray.mosqueda@gmail.com>
2018-05-21 11:35:54 +0800

    modified settings.py to accomodate functionalities

--- Project/settings.py
+++ Project/settings.py
@@ -1,1 +1,17 @@
 from .local_settings import *
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+FIXTURE_DIRS = (
+    '/fixtures/',
+    '/stream/fixtures/',
+)
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
