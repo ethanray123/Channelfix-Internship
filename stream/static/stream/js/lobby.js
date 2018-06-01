f903d9108627261ba75a38101089ab3a5b75bf11
Ethan Ray Flores Mosqueda <ethanray.mosqueda@gmail.com>
2018-06-01 11:16:40 +0800

    removed subscription functionality from lobby.js. moved to subscription.js

--- stream/static/stream/js/lobby.js
+++ stream/static/stream/js/lobby.js
@@ -87,23 +87,3 @@ $(function(){
     });//end of comment function
 
 });
-
-$("button#subscribe").click(function(){
-    var subscribe = $(this);
-    $.ajax({
-        type: 'POST',
-        url: 'subscribe/',
-        data: {
-            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken').val(),
-            'streamer_id': subscribe.attr("name")
-        },
-        success: function(response){
-            console.log(response);
-            if(response){
-                subscribe.text("Unfollow");
-            }else{
-                subscribe.text("Follow");
-            }
-        }
-    });
-});
