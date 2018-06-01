4831f62d37d1e1c35b56e4393b77a13bd6efb08c
rendave <rendave.lecciones3@gmail.com>
2018-06-01 17:15:26 +0800

    imported search.py from api folder

--- stream/api/__init__.py
+++ stream/api/__init__.py
@@ -1,5 +1,5 @@
 from stream.api import (
     lobby, stream, subscription, comment,
     category, report, profile, moderator,
-    lobby_views, lobby_membership
+    lobby_views, lobby_membership, search
 )
