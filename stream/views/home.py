1ee5771028f4dd065bed6c0d98cd1fa5f71f283e
Ethan Ray Flores Mosqueda <ethanray.mosqueda@gmail.com>
2018-06-02 11:16:58 +0800

    set up display notifications in home

--- stream/views/home.py
+++ stream/views/home.py
@@ -8,4 +8,17 @@ class HomeView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['lobbies'] = models.Lobby.objects.all()
+        if(self.request.user.is_authenticated):
+            context['notifications'] = self.get_notifications()
         return context
+
+    def get_notifications(self):
+        queryset = models.Notification.objects.filter(
+            owner=self.request.user).order_by('-when')[:5]
+        results = []
+        for obj in queryset:
+            temp = {}
+            temp['description'] = obj.get_notification
+            temp['when'] = obj.when
+            results.append(temp)
+        return results
